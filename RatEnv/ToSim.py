2eddc09a68363a40062809f88ebe6330bad9a19a
RicardoZon <337875754@qq.com>
2023-08-04 12:16:29 +0800

    UP

--- RatEnv/ToSim.py
+++ RatEnv/ToSim.py
@@ -5,7 +5,8 @@ import math
 from collections import deque
 import scipy.io as scio
 import time
-
+from Controller import MouseController
+import argparse
 
 # 理念：
 # SimModel是对Mouse的虚拟模拟
@@ -172,3 +173,41 @@ class SimModel(object):
 				trajectoryFile.write(str(self.movePath[j][i])+' ')
 			trajectoryFile.write('\n')
 		trajectoryFile.close()
+
+
+if __name__ == '__main__':
+	RUN_STEPS = 10000
+	parser = argparse.ArgumentParser("Description.")
+	parser.add_argument('--fre', default=0.67,
+		type=float, help="Gait stride")
+	args = parser.parse_args()
+
+	theMouse = SimModel("../models/dynamic_4l_t3.xml", Render=True)
+	frame_skip = 1
+	dt = theMouse.model.opt.timestep*frame_skip
+
+	theController = MouseController(args.fre, timestep=dt)
+
+	for i in range(500):
+		ctrlData = [0.0, 1.5, 0.0, 1.5, 0.0, -1.2, 0.0,-1.2, 0,0,0,0]
+		theMouse.runStep(ctrlData)
+	theMouse.initializing()
+	start = time.time()
+
+	for i in range(RUN_STEPS):
+		pos_pre = theMouse.pos.copy()
+		ctrlData = theController.runStep()				# No Spine
+		#tCtrlData = theController.runStep_spine()		# With Spine
+		for _ in range(frame_skip):
+			theMouse.runStep(ctrlData, render=True)
+		pos = theMouse.pos
+
+		v = (pos[1]-pos_pre[1])*(-4)/dt
+		# print(v)
+		# print(pos)
+		print(theMouse.sim.data.get_joint_qpos("knee1_fl"))
+		print(theMouse.sim.data.get_joint_qpos("ankle_fl"))
+
+	end = time.time()
+	timeCost = end-start
+	print("Time -> ", timeCost)
